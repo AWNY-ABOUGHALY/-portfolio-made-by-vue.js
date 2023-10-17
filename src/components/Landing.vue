655963475141e53832e6607d8305edadaf856323
AWNY-ABOUGHALY <awny.h.ghaly@gmail.com>
2023-10-17 20:53:33 +0300

    style(portfolio): :lipstick: reset the height of the landing section

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,22 +1,33 @@
 <template>
-  <div id="landing" class="flex-center pt-10">
+  <div id="landing" class="flex-center pt-10 my-7">
     <div class="container" style="max-width: 900px">
       <v-row justify="center">
-        <v-col cols="12" md="7" class="font-weight-black text-center text-md-start mb-1 mb-md-0">
+        <v-col
+          cols="12"
+          md="7"
+          class="font-weight-black text-center text-md-start mb-1 mb-md-0"
+        >
           <p>I'm</p>
           <p class="blue-grey--text text--darken-3">Awny</p>
           <p class="blue-grey--text text--darken-3">Aboughaly</p>
           <p>Frontend Developer</p>
         </v-col>
-        <v-col cols="12" md="5" class="d-flex justify-center" style="height: 350px;">
-          <img v-for="img in imgs" :key="img.id"
+        <v-col
+          cols="12"
+          md="5"
+          class="d-flex justify-center"
+          style="height: 350px"
+        >
+          <img
+            v-for="img in imgs"
+            :key="img.id"
             :src="`/imgs/${img.image}`"
-            :alt="img.image.slice(0,-4)"
-            :width="205 + (img.id * 35)"
+            :alt="img.image.slice(0, -4)"
+            :width="205 + img.id * 35"
             height="155"
-            :style="`margin-top:${0 + ((img.id - 1) * 3)}rem;
+            :style="`margin-top:${0 + (img.id - 1) * 3}rem;
                     transform: rotate(${img.rotate}deg);
-                    max-width: ${65 + (img.id * 5)}%`"
+                    max-width: ${65 + img.id * 5}%`"
           />
         </v-col>
       </v-row>
@@ -27,7 +38,7 @@
 <script>
 export default {
   name: "Landing",
-  data(){
+  data() {
     return {
       imgs: [
         { id: 1, image: "work1.png", rotate: 10 },
@@ -35,30 +46,34 @@ export default {
         { id: 3, image: "work3.png", rotate: 10 },
         { id: 4, image: "work4.png", rotate: -5 },
         { id: 5, image: "work5.png", rotate: 5 },
-      ]
-    }
+      ],
+    };
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 #landing {
-  min-height: 100vh;
+  @media screen and (min-width: 960px) {
+    height: 100vh;
+    max-height: 800px;
+  }
   p {
     line-height: 4.3rem;
-    @media (max-width: 600px){
+    @media (max-width: 600px) {
       line-height: 2.8rem;
     }
     &:not(:first-child, :last-child) {
       font-size: 4rem;
-      @media (max-width: 600px){
+      @media (max-width: 600px) {
         font-size: 2.8rem;
       }
     }
-    &:first-child, &:last-child {
+    &:first-child,
+    &:last-child {
       color: #ad7f59;
       font-size: 2.8rem;
-      @media (max-width: 600px){
+      @media (max-width: 600px) {
         font-size: 1.8rem;
       }
     }
