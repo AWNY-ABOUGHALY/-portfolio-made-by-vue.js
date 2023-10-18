320d969528d0b6bba32d3b3ba003a6a188735418
AWNY-ABOUGHALY <awny.h.ghaly@gmail.com>
2023-10-18 21:01:01 +0300

    feat(portfolio): :recycle: change the name title of portfolio  in header to be projects instead

--- src/components/Layout/NavbarAll.vue
+++ src/components/Layout/NavbarAll.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="navbarall">
-    <v-app-bar
-      fixed
-      height="56"
-      color="#3F4E4F"
-      dark
-      elevate-on-scroll
-    >
+    <v-app-bar fixed height="56" color="#3F4E4F" dark elevate-on-scroll>
       <img
         class="me-2 mb-1"
         alt="Awny Aboughaly Portfolio"
@@ -14,12 +8,21 @@
         width="58"
         height="54"
       />
-      <v-toolbar-title class="hidden-sm-and-up" v-if="!drawer">Awny Aboughaly</v-toolbar-title>
+      <v-toolbar-title class="hidden-sm-and-up" v-if="!drawer"
+        >Awny Aboughaly</v-toolbar-title
+      >
       <v-spacer></v-spacer>
       <v-btn
-        v-for="navItem in navItems" :key="navItem.id" :href="navItem.path"
+        v-for="navItem in navItems"
+        :key="navItem.id"
+        :href="navItem.path"
         class="hidden-xs-only"
-        text plain rounded dark active-class="active" exact-active-class="active"
+        text
+        plain
+        rounded
+        dark
+        active-class="active"
+        exact-active-class="active"
       >
         {{ navItem.title }}
       </v-btn>
@@ -30,14 +33,10 @@
       >
       </v-app-bar-nav-icon>
     </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      right
-      temporary
-    >
+    <v-navigation-drawer v-model="drawer" fixed right temporary>
       <v-list>
-        <v-toolbar-title v-if="drawer"
+        <v-toolbar-title
+          v-if="drawer"
           class="sidebartitle text-center font-weight-bold"
         >
           <span class="d-inline-block pb-1">Awny Aboughaly</span>
@@ -45,9 +44,12 @@
         <v-list-item
           v-for="navItem in navItems"
           :key="navItem.id"
-          :class="`${+navItem.id === 1 ? 'mt-8':''}`"
-          :href="navItem.path">
-          <v-list-item-title class="font-weight-bold text-center">{{ navItem.title }}</v-list-item-title>
+          :class="`${+navItem.id === 1 ? 'mt-8' : ''}`"
+          :href="navItem.path"
+        >
+          <v-list-item-title class="font-weight-bold text-center">{{
+            navItem.title
+          }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -56,24 +58,24 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       drawer: false,
       navItems: [
         { id: "1", title: "Home", path: "#landing" },
         { id: "2", title: "Resume", path: "#resume" },
-        { id: "3", title: "Portfolio", path: "#portfolio" },
+        { id: "3", title: "Projects", path: "#projects" },
         { id: "4", title: "Contact", path: "#contact" },
-      ]
-    }
+      ],
+    };
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 .navbarall {
   .sidebartitle span {
-    border-bottom: 2px solid #E0A96D;
+    border-bottom: 2px solid #e0a96d;
   }
 }
 </style>
